e4f4c2481abad69388d28a213f63d5066a84970e
DESKTOP-ELDPJU4\Sally <sallyliviak.1807@gmail.com>
2022-03-19 01:30:04 +0700

    Add project files.

---
+++ koneksi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Tugas3_PV_Kasir_Sally_Livia_Kosasih_201401025
+{
+    class koneksi
+    {
+        public SqlConnection GetConn() 
+        {
+            SqlConnection Conn = new SqlConnection();
+            Conn.ConnectionString = "Data Source= DESKTOP-ELDPJU4\\SQLEXPRESS; Initial Catalog = Kasir; Integrated Security=true";
+            return Conn;
+        }
+        
+    }
+}
