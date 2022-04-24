81b5e91a858e37988d4681a77d6ac29dd9df4656
sallyliviakosasih <sallyliviak.1807@gmail.com>
2022-04-24 13:09:48 +0700

    Commit Changed

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,36 +29,221 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 182);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 270);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 29;
-            this.dataGridView1.Size = new System.Drawing.Size(1009, 188);
+            this.dataGridView1.Size = new System.Drawing.Size(1009, 274);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kode Barang";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nama Barang";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Harga Beli";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Harga Jual";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Jumlah";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 20);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Satuan";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 57);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(903, 27);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(118, 90);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 27);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(118, 123);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(169, 27);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(118, 156);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(169, 27);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(118, 189);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(169, 27);
+            this.textBox6.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 247);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(214, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "DB : Kasir, Tabel : TBL_BARANG";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(304, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Simpan";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_simpan);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(393, 186);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 32);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(489, 186);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 32);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Hapus";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1033, 382);
+            this.ClientSize = new System.Drawing.Size(1033, 556);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private DataGridView dataGridView1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Label label7;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -27,6 +27,12 @@ namespace Tugas3_PV_Kasir_Sally_Livia_Kosasih_201401025
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            Tampil_Barang();
+            Bersihkan();
+        }
+        //menampilkan tabel
+        void Tampil_Barang()
+        {
             SqlConnection conn = Konn.GetConn();
             try
             {
@@ -48,6 +54,121 @@ namespace Tugas3_PV_Kasir_Sally_Livia_Kosasih_201401025
                 conn.Close();
             }
         }
+        //reset kolom
+        void Bersihkan()
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
+            textBox6.Text = "";
+        }
 
+        //Membuat tombol Simpan
+        private void button_simpan(object sender, EventArgs e)
+        {
+            //Memeriksa apakah tombol text kosong
+            if(textBox1.Text.Trim()  == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" || textBox4.Text.Trim() == "" || textBox5.Text.Trim() == "" || textBox6.Text.Trim()== "")
+            {
+                MessageBox.Show("Kolom tidak boleh Kosong");
+            }
+            else
+            {
+                //Menyimpan data yang diinput
+                SqlConnection conn = Konn.GetConn();
+                try
+                {
+                    cmd = new SqlCommand("INSERT INTO TBL_BARANG VALUES ('" + textBox1.Text + "','"+textBox2.Text+"','"+textBox3.Text+"','"+textBox4.Text+ "','" + textBox5.Text + "','" + textBox6.Text + "')", conn);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data Berhasil Ditambahkan");
+                    Tampil_Barang();
+                    Bersihkan();
+                }
+                catch(Exception X)
+                {
+                    MessageBox.Show(X.ToString());
+                    Bersihkan();
+                }
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+                textBox1.Text = row.Cells["KodeBarang"].Value.ToString();
+                textBox2.Text = row.Cells["NamaBarang"].Value.ToString();
+                textBox3.Text = row.Cells["HargaJual"].Value.ToString();
+                textBox4.Text = row.Cells["HargaBeli"].Value.ToString();
+                textBox5.Text = row.Cells["JumlahBarang"].Value.ToString();
+                textBox6.Text = row.Cells["SatuanBarang"].Value.ToString();
+            }
+            catch (Exception X)
+            {
+                MessageBox.Show(X.ToString());
+            }
+        }
+        //update data
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Memeriksa apakah tombol text kosong
+            if (textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" || textBox4.Text.Trim() == "" || textBox5.Text.Trim() == "" || textBox6.Text.Trim() == "")
+            {
+                MessageBox.Show("Kolom tidak boleh Kosong");
+            }
+            else
+            {
+                //Menyimpan data yang diinput
+                SqlConnection conn = Konn.GetConn();
+                try
+                {
+                    cmd = new SqlCommand("UPDATE TBL_BARANG SET KodeBarang = '" + textBox1.Text + "', NamaBarang = '" + textBox2.Text + "', HargaJual = '" + textBox3.Text + "', HargaBeli = '" + textBox4.Text + "', JumlahBarang = '" + textBox5.Text + "', SatuanBarang = '" + textBox6.Text + "'WHERE KodeBarang = '"+textBox1.Text+"'", conn);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Data Berhasil Diupdate");
+                    Tampil_Barang();
+                    Bersihkan();
+                }
+                catch (Exception X)
+                {
+                    MessageBox.Show(X.ToString());
+                    Bersihkan();
+                }
+            }
+        }
+
+        //hapus data
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Memeriksa apakah tombol text kosong
+            if (textBox1.Text.Trim() == "" || textBox2.Text.Trim() == "" || textBox3.Text.Trim() == "" || textBox4.Text.Trim() == "" || textBox5.Text.Trim() == "" || textBox6.Text.Trim() == "")
+            {
+                MessageBox.Show("Data tidak dapat dihapus");
+            }
+            else
+            {
+                if (MessageBox.Show("Apakah Anda Ingin Menghapus Data?", "Hapus Data", MessageBoxButtons.YesNo, MessageBoxIcon.Information) == DialogResult.Yes)
+                {
+                    SqlConnection conn = Konn.GetConn();
+                    try
+                    {
+                        cmd = new SqlCommand("DELETE FROM TBL_BARANG WHERE KodeBarang = '" + textBox1.Text + "'", conn);
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Data Berhasil dihapus");
+                        Tampil_Barang();
+                        Bersihkan();
+                    }
+                    catch (Exception X)
+                    {
+                        MessageBox.Show(X.ToString());
+                        Bersihkan();
+                    }
+                }
+            }
+        }
     }
 }
